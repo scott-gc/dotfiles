29f77520533ca930a16813b48eb1adec77a84635
Scott Gibson-Craig <scottgc@Scotts-MacBook-Pro.local>
2017-05-04 15:03:33 -0600

    Making changes to the content of my dotfiles.

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,7 +1,10 @@
 # grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
+# Made possible through contributions from generous benefactors like
+# `brew install coreutils`
   alias ls="ls -F -color"
   alias l="ls -la -color"
   alias ll="ls -l -color"
   alias la='ls -A -color'
+
+# Pipe my public key to my clipboard.
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,1 @@
-
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
