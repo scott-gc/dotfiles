78ed425457859f74fe02f67ae4ca6c984dd324b1
Scott Gibson-Craig <scott@gibson-craig.com>
2018-04-25 14:15:20 -0600

    update dotfiles.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,7 +10,7 @@ fi
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
-alias gd='git diff'
+alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
@@ -18,3 +18,7 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
+
+alias gcb='git copy-branch-name'
+alias gac='git add -A && git commit -m'
+alias ge='git-edit-new'
