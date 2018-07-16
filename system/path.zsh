362c4245978e6b261ba6f92160bbf4f11320ba19
Scott Gibson-Craig <scott@gibson-craig.com>
2018-07-16 11:34:38 -0600

    update dotfiles.

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,4 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PKG_CONFIG_PATH=/usr/local/Cellar/imagemagick@6/6.9.10-4/lib/pkgconfig
+export PATH="/usr/local/opt/postgresql@9.5/bin:$PATH"
