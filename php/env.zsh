dc3f7ea65dfa0b8ca998bb3a5853ead047790360
scottgc <scott.gc@gmail.com>
2019-03-13 15:59:25 -0600

    Update the php topic to include postgresql.

--- php/env.zsh
+++ php/env.zsh
@@ -1,1 +1,1 @@
-export PHP_BUILD_CONFIGURE_OPTS="--with-openssl=$(brew --prefix openssl) --with-libzip"
+export PHP_BUILD_CONFIGURE_OPTS="--with-openssl=$(brew --prefix openssl) --with-libzip --with-pgsql=$(brew --prefix postgresql) --with-pdo-pgsql=$(brew --prefix postgresql)"
