0bddbd0f12cc1a3b68fb0d706cfe9cd992a46de7
scottgc <scott.gc@gmail.com>
2019-03-27 11:12:50 -0600

    Update zsh topic to use oh-my-zsh.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,8 @@
-alias reload!='. ~/.zshrc'
 alias ls="ls -F -color"
 alias l="ls -la -color"
 alias ll="ls -l -color"
 alias la='ls -A -color'
+
+alias reload!='. ~/.zshrc'
+alias zshconfig="vim ~/.zshrc"
+alias ohmyzsh="vim ~/.oh-my-zsh"
